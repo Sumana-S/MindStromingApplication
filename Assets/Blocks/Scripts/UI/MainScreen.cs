58fd8b977389008645c1b046d87e4cb23177b749
Sumana_S <59315290+Sumana-S@users.noreply.github.com>
2021-08-31 20:18:44 +0530

    Add files via upload

---
+++ Assets/Blocks/Scripts/UI/MainScreen.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BizzyBeeGames.Blocks
+{
+	public class MainScreen : Screen
+	{
+		#region Inspector Variables
+
+		//[SerializeField] private GameObject	removeAdsButton = null;
+
+		#endregion
+
+		#region Unity Methods
+
+		protected override void Start()
+		{
+			base.Start();
+
+			//bool adsRemoved = MobileAdsManager.Instance.AdsRemoved;
+
+			//removeAdsButton.SetActive(!adsRemoved);
+
+			//if (!adsRemoved)
+			//{
+				//MobileAdsManager.Instance.OnAdsRemoved += () => { removeAdsButton.SetActive(false); };
+			//}
+		}
+
+		#endregion
+	}
+}
